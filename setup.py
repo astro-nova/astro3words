a90c8bbc86ddf4b4e014c10dac5d4d5a3ff73ace
Liza Sazonova <esazono1@jhu.edu>
2021-06-23 23:55:00 +0100

    add ignores

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
-from os import name
+
 from setuptools import setup, find_packages
+import numpy
+
+# borrowed from orbitize: https://github.com/sblunt/orbitize
+def get_requires():
+    reqs = []
+    for line in open('requirements.txt', 'r').readlines():
+        reqs.append(line)
+    return reqs
 
 setup(
-    name = "Astro3Words",
-    version = "0.1",
-    packages = find_packages()
-)
+    name='astro3words',
+    version='0.3',
+    description='astro3words - get a simple 3-word coordinate of your favorite object!',
+    url='https://github.com/astro-nova/astro-3-words',
+    author='',
+    author_email='',
+    license='MIT',
+    packages=find_packages(),
+    include_dirs=[numpy.get_include()],
+    include_package_data = True,
+    zip_safe=False,
+    classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+
+        # Pick your license as you wish (should match "license" above)
+        # 'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 3',
+        ],
+    keywords='Astronomy Astrometry',
+    install_requires=get_requires()
+    )
+
